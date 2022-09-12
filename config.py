eca32f9afb479babc3d7b7736d3fb96058fc1498
StrekhaIgor <is1989@yandex.ru>
2022-09-12 23:27:28 +0300

    add reset password

--- config.py
+++ config.py
@@ -10,4 +10,10 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
-    POSTS_PER_PAGE = 25
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.yandex.ru'
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['qchess-server@yandex.ru', 'is1989@yandex.ru']
+    POSTS_PER_PAGE = 10
