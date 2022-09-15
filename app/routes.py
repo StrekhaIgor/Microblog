e67657e94248f3ee303a8e8cb78076e777474e66
StrekhaIgor <is1989@yandex.ru>
2022-09-15 23:34:02 +0300

    bootstrap

--- app/routes.py
+++ app/routes.py
@@ -7,8 +7,6 @@ from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Post
 from werkzeug.urls import url_parse
 from datetime import datetime
-from flask_mail import Message
-from app import mail
 
 
 @app.before_request
