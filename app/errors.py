9bd519621a8c3f3b837ac9be76b0c56e9eb79869
StrekhaIgor <is1989@yandex.ru>
2022-10-02 15:59:43 +0300

    add ru translation interfase

--- app/errors.py
+++ app/errors.py
@@ -1,12 +1,11 @@
-from xml.dom.pulldom import ErrorHandler
 from flask import render_template
 from app import app, db
-import logging
-from logging.handlers import SMTPHandler
+
 
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_error(error):
